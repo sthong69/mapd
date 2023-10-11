dd23b816ca01d242def96890aec66f04cddd41b1
ArthurNérambourg <arthur.nerambourg64@gmail.com>
2023-10-11 15:59:20 +0200

    trans

--- src/MainPackage/Transition.java
+++ src/MainPackage/Transition.java
@@ -1,5 +1,43 @@
 package MainPackage;
 
-public class Transition {
+import java.util.LinkedList;
 
+public class Transition {
+	
+	private LinkedList<ArcIn> arcInList;
+	private LinkedList<ArcOut> arcOutList;
+	
+	
+	public Transition() {
+		setArcInList();
+		setArcOutList();
+	}
+	public LinkedList<ArcIn> getArcInList() {
+		return arcInList;
+	}
+	public void setArcInList() {
+		this.arcInList = new LinkedList<ArcIn>();
+	}
+	public LinkedList<ArcOut> getArcOutList() {
+		return arcOutList;
+	}
+	public void setArcOutList() {
+		this.arcOutList = new LinkedList<ArcOut>();
+	}
+	
+	public boolean isDrawable() {
+		boolean test;
+		for (ArcIn arc : this.arcInList) {
+			test = arc.checkAvailability();
+			if (!test) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public String toString() {
+		return "Transition String";
+	}
+	
 }
