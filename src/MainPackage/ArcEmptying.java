ead429b72a995cf81b27f13b084f538bec5dc58f
Sylvain <sylvain.thong@imt-atlantique.net>
2023-10-11 14:44:08 +0200

    new Arcs

---
+++ src/MainPackage/ArcEmptying.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class ArcEmptying extends ArcOut {
+
+	public ArcEmptying(int weight, Place place, Transition transition) {
+		super(weight, place, transition);
+	}
+
+}
