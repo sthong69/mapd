f0e6256e687d928630e10a7beead8b9fc6f630f2
ArthurNérambourg <arthur.nerambourg64@gmail.com>
2023-10-11 14:59:10 +0200

    des changement

--- src/MainPackage/PetriNetInterface.java
+++ src/MainPackage/PetriNetInterface.java
@@ -1,5 +1,14 @@
 package MainPackage;
 
-public interface PetriNetInterface {
-
+import java.util.LinkedList;public interface PetriNetInterface {
+	public void fire();
+	public void addArc(int weight, Place p, Transition t);
+	public void addPlace(int tokens);
+	public void addTransition(LinkedList<ArcIn> arcIn, LinkedList<ArcOut> arcOut);
+	public void removePlace(Place p);
+	public void removeArc(Arc a);
+	public void removeTransition(Transition t);
+	public String toString();
+	
+	
 }
