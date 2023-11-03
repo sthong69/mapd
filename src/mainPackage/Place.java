1d3545b53d8b3bb3c9dfa6bf831ddbd349fa4219
Sylvain <sylvain.thong@imt-atlantique.net>
2023-11-03 14:35:50 +0100

    complete TestPlace

--- src/mainPackage/Place.java
+++ src/mainPackage/Place.java
@@ -30,15 +30,18 @@ public class Place {
 	}
 	
 	public void addTokens(int nbTokensToAdd) throws NegativeNbTokensException {
-		if (nbTokensToAdd<0) {
+		if (nbTokensToAdd < 0) {
 			throw new NegativeNbTokensException("La quantité de jetons à ajouter ne peut pas être négative !");
 		}
 		nbTokens += nbTokensToAdd;
 	}
 	
-	public void removeTokens(int nbTokensToRemove) throws Exception {
+	public void removeTokens(int nbTokensToRemove) throws NegativeNbTokensException {
+		if (nbTokensToRemove < 0) {
+			throw new NegativeNbTokensException("La quantité de jetons à enlever ne peut pas être négative !");
+		}
 		if (nbTokensToRemove > nbTokens) {
-			throw new Exception("Pas assez de jetons.");
+			throw new NegativeNbTokensException("La quantité de jetons finale ne peut pas être négative !");
 		}
 		nbTokens -= nbTokensToRemove;
 	}
@@ -50,10 +53,4 @@ public class Place {
 	public String toString() {
 		return "";
 	}
-	
-	public static void main(String[] args) {
-		Place p0 = new Place(2);
-		System.out.println(p0.toString());
-	}
-	
 }
--- src/tests/TestPlace.java
+++ src/tests/TestPlace.java
@@ -19,21 +19,46 @@ public class TestPlace {
 		p0 = new Place(0);
 	}
 	
-	@Test
-	@Order(1)  
+	@Test  
 	public void testConstructor() {
 		p0 = new Place(0);
-		System.out.println(p0.getNbTokens());
-		System.out.println(p0.getArcList().size());
 		Assertions.assertEquals(0,p0.getNbTokens());
 		Assertions.assertEquals(0,p0.getArcList().size());
 	}
 	
 	@Test
-	@Order(2)
-	public void testAddTokens() throws Exception {
+	public void testAddTokens() throws NegativeNbTokensException {
+		p0 = new Place(0);
+		// CAJ0
+		NegativeNbTokensException exceptionMessage = Assertions.assertThrows(NegativeNbTokensException.class, ()->{p0.addTokens(-17);});
+		Assertions.assertEquals("La quantité de jetons à ajouter ne peut pas être négative !",exceptionMessage.getMessage());
+		
+		// CAJ1
 		p0.addTokens(17);
 		Assertions.assertEquals(17, p0.getNbTokens());
-		Assertions.assertThrows(NegativeNbTokensException.class, ()->p0.addTokens(-17));
+	}
+	
+	@Test
+	public void testRemoveTokens() throws NegativeNbTokensException {
+		p0 = new Place(17);
+		// CEJ0
+		NegativeNbTokensException exceptionMessage0 = Assertions.assertThrows(NegativeNbTokensException.class, ()->{p0.removeTokens(-17);});
+		Assertions.assertEquals("La quantité de jetons à enlever ne peut pas être négative !",exceptionMessage0.getMessage());
+		
+		// CEJ1
+		NegativeNbTokensException exceptionMessage1 = Assertions.assertThrows(NegativeNbTokensException.class, ()->{p0.removeTokens(18);});
+		Assertions.assertEquals("La quantité de jetons finale ne peut pas être négative !",exceptionMessage1.getMessage());
+		
+		// CEJ2
+		p0.removeTokens(7);
+		Assertions.assertEquals(10,p0.getNbTokens());
+	}
+	
+	@Test
+	public void testIsEmpty() throws NegativeNbTokensException {
+		p0 = new Place(0);
+		Assertions.assertTrue(p0.isEmpty());
+		p0.addTokens(1);
+		Assertions.assertFalse(p0.isEmpty());
 	}
 }
